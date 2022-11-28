fcc41b5cce5ea633e5b4a9bab234889eb48efb17
Anna1606 <barsukova.a1990@yandex.ru>
2022-11-29 02:35:21 +0300

    Task _Friends

---
+++ Task _Friends/Program.cs
@@ -0,0 +1,27 @@
+﻿int distanse = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int friend = 2;
+
+int TimeCode = 0;
+int count = 0;
+
+while(distanse > 10)
+{
+    if(friend == 1)
+    { 
+        TimeCode = distanse / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        TimeCode = distanse / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+
+    distanse = distanse - (FirstFriendSpeed + SecondFriendSpeed) * TimeCode;
+    count++;
+ }
+
+ Console.WriteLine(count);
